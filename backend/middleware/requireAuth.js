e33d734731f7d521a2f2f503b1f093887f40762a
Ladzsz <londonmason602@gail.com>
2025-12-17 13:07:49 -0500

    created middleware for auth

---
+++ backend/middleware/requireAuth.js
@@ -0,0 +1,32 @@
+import { verifyToken } from "../utils/jwt.js"
+import { users } from "../controllers/usercontroller.js"
+
+export const requireAuth = (req, res, next) => {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Unauthorized" })
+  }
+
+  const token = authHeader.split(" ")[1]
+
+  try {
+    
+    const decoded = verifyToken(token)
+
+    const user = users.find(u => u.id === decoded.id)
+    if (!user) {
+      return res.status(401).json({ error: "User not found" })
+    }
+
+    req.user = {
+      id: user.id,
+      email: user.email,
+      role: user.role
+    }
+
+    next()
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" })
+  }
+}
