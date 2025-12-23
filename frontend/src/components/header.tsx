137d89db7ec935e0d51398c5ce906be2b779341b
Ladzsz <londonmason602@gail.com>
2025-12-23 15:11:21 -0500

    fixed mobile header styling

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -4,16 +4,20 @@ import "../assets/styles/headerstyles.css";
 
 export default function Header() {
   return (
-    <header className="site-header">
-      <Link to="/" className="site-header__logo">
-        Store
-      </Link>
+    <header className={`site-header`}>
+      <div className="header-left">
+        <Link to="/" className="site-header__logo">
+          Store
+        </Link>
 
-      <SearchBar />
+        <nav className="site-header__nav">
+          <Link to="/cartPage">MyCart</Link>
+        </nav>
+      </div>
 
-      <nav className="site-header__nav">
-        <Link to="/cartPage">MyCart</Link>
-      </nav>
+      <div className="header-right">
+        <SearchBar />
+      </div>
     </header>
   );
 }
